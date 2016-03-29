95963223f1fb5be634090315da1f44241a55302f
Tim Spinks <tim@ninthlink.com>
2016-03-29 14:49:04 -0700

    conditional to render container only if ID exists

--- gtm4wp.php
+++ gtm4wp.php
@@ -107,15 +107,17 @@ function gtm4wp_container_output() {
 	$options = get_option( 'gtm4wp_settings' );
 	$container_id = $options['gtm4wp_container_id'];
 
-	printf( "<!-- Google Tag Manager -->
-		<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=%s\"
-		height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','%s');</script>
-		<!-- End Google Tag Manager -->", $container_id, $container_id );
+	if ( !empty($container_id) ) {
+		printf( "<!-- Google Tag Manager -->
+			<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=%s\"
+			height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','%s');</script>
+			<!-- End Google Tag Manager -->", $container_id, $container_id );
+	}
 
 }
 
