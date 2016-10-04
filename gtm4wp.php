857d34e92dc841ca165a5f871482a86b5fafb9fd
Tim Spinks <tim@ninthlink.com>
2016-10-04 11:39:11 -0700

    updated fields for brand

--- gtm4wp.php
+++ gtm4wp.php
@@ -51,6 +51,13 @@ function gtm4wp_settings_init(  ) {
 		'pluginPage', 
 		'gtm4wp_pluginPage_section' 
 	);
+	add_settings_field( 
+		'gtm4wp_brand', 
+		__( 'Brand', 'gtm4wp' ), 
+		'gtm4wp_brand_render', 
+		'pluginPage', 
+		'gtm4wp_pluginPage_section' 
+	);
 }
 
 
@@ -58,6 +65,12 @@ function gtm4wp_container_id_render(  ) {
 	$options = get_option( 'gtm4wp_settings' );
 	?>
 	<input type='text' name='gtm4wp_settings[gtm4wp_container_id]' value='<?php echo $options['gtm4wp_container_id']; ?>'>
+	<?php
+}
+function gtm4wp_brand_render(  ) { 
+	$options = get_option( 'gtm4wp_settings' );
+	?>
+	<input type='text' name='gtm4wp_settings[gtm4wp_brand]' value='<?php echo $options['gtm4wp_brand']; ?>'>
 	<?php
 }
 
@@ -119,151 +132,152 @@ function gtm4wp_datalayer_init() {
  */
 add_action( 'wp_footer', 'woo_data_layer' );
 function woo_data_layer() {
-    if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-        global $woocommerce;
-        $category_slug = get_query_var( 'product_cat' );
+	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+		global $woocommerce;
+		$options = get_option( 'gtm4wp_settings' );
+		$brand = sanitize_text_field( $options['gtm4wp_brand'] );
+		$category_slug = get_query_var( 'product_cat' );
 
-        echo '<pre>'; print_r( $category_slug ); echo '</pre>';
+		echo '<pre>'; print_r( $category_slug ); echo '</pre>';
 
-        $brand = 'Cabeau';
-        // CART page
-        if ( is_cart() ):
-            $str = '<script>dataLayer.push({"cart" : [';
-            foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-                //print_r($cart_item);
-                //print_r(WC()->cart->get_item_data($cart_item, true));
-                $_product     = $cart_item['data'];
-                $product_id   = $cart_item['product_id'];
-                $str .= sprintf('{
-                        "name" : "%s",
-                        "price" : %f,
-                        "quantity" : %d
-                    },', $_product->post->post_title, WC()->cart->get_product_price( $_product ), $cart_item['quantity']);
-            }
-            $str .= ']});</script>';
-        endif;
-        // PRODUCT CATEGORY
-        if ( is_product_category() ):
-            $i = 1;
-            $category                   = get_term_by( 'slug', $category_slug, 'product_cat' );
-            var_dump($category);
-            // Get all products in category
-            $args = array(
-                'post_type'             => 'product',
-                'post_status'           => 'publish',
-                'posts_per_page'        => -1,
-                'product_cat'           => $category_slug
-            );
-            $products = new WP_Query($args);
-            echo '<pre>'; print_r($products); echo '</pre>';
-            $str = '<script>
-                dataLayer.push({
-                    \'event\':\'enhanceEcom Product Impression\',
-                    \'ecommerce\': {
-                        \'impressions\': [';
-            while ( $products->have_posts() ) : $products->the_post(); 
-                global $product;
-                $str .= sprintf('{
-                    \'name\': \'%s\',
-                    \'id\': %d,
-                    \'price\': %f,
-                    \'brand\': \'%s\',
-                    \'category\': \'%s\',
-                    \'list\': \'%s\',
-                    \'position\': %d
-                    },', get_the_title(), get_the_ID(), $product->get_price(), $brand, $category, $category, $i);
-                $i++;
-            endwhile;
-            wp_reset_query();
-            $str .= ']}});</script>';
-        endif;
-        // PRODUCT CATEGORY - Travel Pillows
-        if ( is_page(6608) ):
-            $i = 1;
-            $category                   = 'Travel Pillows';
-            $category_slug              = 'travel-pillows';
-            // Get all products in category
-            $args = array(
-                'post_type'             => 'product',
-                'post_status'           => 'publish',
-                'posts_per_page'        => -1,
-                'product_cat'           => $category_slug
-            );
-            $products = new WP_Query($args);
-            echo '<pre>'; print_r($products); echo '</pre>';
-            $str = '<script>
-                dataLayer.push({
-                    \'event\':\'enhanceEcom Product Impression\',
-                    \'ecommerce\': {
-                        \'impressions\': [';
-            while ( $products->have_posts() ) : $products->the_post(); 
-                global $product;
-                $str .= sprintf('{
-                    \'name\': \'%s\',
-                    \'id\': %d,
-                    \'price\': %f,
-                    \'brand\': \'%s\',
-                    \'category\': \'%s\',
-                    \'list\': \'%s\',
-                    \'position\': %d
-                    },', get_the_title(), get_the_ID(), $product->get_price(), $brand, $category, $category, $i);
-                $i++;
-            endwhile;
-            wp_reset_query();
-            $str .= ']}});</script>';
-        endif;
-        // PRODUCT CATEGORY - Comfort
-        if ( is_page(6785) ):
-            var_dump(WC());
-        endif;
-        // PRODUCT CATEGORY - Accessories
-        if ( is_page(6788) ):
-            var_dump(WC());
-        endif;
-        // PRODUCT pages
-        if ( is_product() ):
-            $product = wc_get_product( get_the_ID() );
-            $price = $product->get_price();
-            $str = sprintf('<script>dataLayer.push({"product" : {
-            	"id" : "%s",
-                "name" : "%s",
-                "price" : %f
-            }})</script>', get_the_ID(), get_the_title(), $price);
-        endif;
-        // ORDER RECEIVED page
-        if( is_wc_endpoint_url( 'order-received' ) ):
-            $order = woo_order_obj();
-            //var_dump($order);
-            $items = $order->get_items();
-            $str = sprintf('<script>dataLayer.push({
-                "order" : {
-                    "id" : %d,
-                    "email" : "%s",
-                    "country" : "%s",
-                    "currency" : "%s",
-                    "total" : %f,
-                    "discounts" : %f,
-                    "shipping-total" : %f,
-                    "tax-total" : %f,
-                    "est-ship-date" : "%s",
-                    "est-delivery-date" : "%s",
-                    "has-preorder" : "",
-                    "has-digital" : "",
-                },', $order->get_order_number(), $order->billing_email, $order->billing_country, $order->order_currency, $order->order_total, $order->cart_discount, $order->order_shipping, $order->order_tax, date('Y-m-d', strtotime('+1 weekday')), date('Y-m-d', strtotime('+6 weekday')));
-            $str .= '"items" : [';
-            foreach ( $items as $item ) {
-                $str .= sprintf('{
-                        "name" : "%s",
-                        "price" : %f,
-                        "quantity" : %d
-                    },', $item['name'], number_format((float)$item['line_total'], 2, '.', ''), $item['qty']);
-            }
-            $str .= ']';
-            $str .= '});</script>';
-        endif;
-        // print script
-        print($str);
-    } else {
-        return false;
-    }
+		// CART page
+		if ( is_cart() ):
+			$str = '<script>dataLayer.push({"cart" : [';
+			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+				//print_r($cart_item);
+				//print_r(WC()->cart->get_item_data($cart_item, true));
+				$_product	 = $cart_item['data'];
+				$product_id   = $cart_item['product_id'];
+				$str .= sprintf('{
+						"name" : "%s",
+						"price" : %f,
+						"quantity" : %d
+					},', $_product->post->post_title, WC()->cart->get_product_price( $_product ), $cart_item['quantity']);
+			}
+			$str .= ']});</script>';
+		endif;
+		// PRODUCT CATEGORY
+		if ( is_product_category() ):
+			$i = 1;
+			$category				   = get_term_by( 'slug', $category_slug, 'product_cat' );
+			var_dump($category);
+			// Get all products in category
+			$args = array(
+				'post_type'			 => 'product',
+				'post_status'		   => 'publish',
+				'posts_per_page'		=> -1,
+				'product_cat'		   => $category_slug
+			);
+			$products = new WP_Query($args);
+			echo '<pre>'; print_r($products); echo '</pre>';
+			$str = '<script>
+				dataLayer.push({
+					\'event\':\'enhanceEcom Product Impression\',
+					\'ecommerce\': {
+						\'impressions\': [';
+			while ( $products->have_posts() ) : $products->the_post(); 
+				global $product;
+				$str .= sprintf('{
+					\'name\': \'%s\',
+					\'id\': %d,
+					\'price\': %f,
+					\'brand\': \'%s\',
+					\'category\': \'%s\',
+					\'list\': \'%s\',
+					\'position\': %d
+					},', get_the_title(), get_the_ID(), $product->get_price(), $brand, $category, $category, $i);
+				$i++;
+			endwhile;
+			wp_reset_query();
+			$str .= ']}});</script>';
+		endif;
+		// PRODUCT CATEGORY - Travel Pillows
+		if ( is_page(6608) ):
+			$i = 1;
+			$category				   = 'Travel Pillows';
+			$category_slug			  = 'travel-pillows';
+			// Get all products in category
+			$args = array(
+				'post_type'			 => 'product',
+				'post_status'		   => 'publish',
+				'posts_per_page'		=> -1,
+				'product_cat'		   => $category_slug
+			);
+			$products = new WP_Query($args);
+			echo '<pre>'; print_r($products); echo '</pre>';
+			$str = '<script>
+				dataLayer.push({
+					\'event\':\'enhanceEcom Product Impression\',
+					\'ecommerce\': {
+						\'impressions\': [';
+			while ( $products->have_posts() ) : $products->the_post(); 
+				global $product;
+				$str .= sprintf('{
+					\'name\': \'%s\',
+					\'id\': %d,
+					\'price\': %f,
+					\'brand\': \'%s\',
+					\'category\': \'%s\',
+					\'list\': \'%s\',
+					\'position\': %d
+					},', get_the_title(), get_the_ID(), $product->get_price(), $brand, $category, $category, $i);
+				$i++;
+			endwhile;
+			wp_reset_query();
+			$str .= ']}});</script>';
+		endif;
+		// PRODUCT CATEGORY - Comfort
+		if ( is_page(6785) ):
+			var_dump(WC());
+		endif;
+		// PRODUCT CATEGORY - Accessories
+		if ( is_page(6788) ):
+			var_dump(WC());
+		endif;
+		// PRODUCT pages
+		if ( is_product() ):
+			$product = wc_get_product( get_the_ID() );
+			$price = $product->get_price();
+			$str = sprintf('<script>dataLayer.push({"product" : {
+				"id" : "%s",
+				"name" : "%s",
+				"price" : %f
+			}})</script>', get_the_ID(), get_the_title(), $price);
+		endif;
+		// ORDER RECEIVED page
+		if( is_wc_endpoint_url( 'order-received' ) ):
+			$order = woo_order_obj();
+			//var_dump($order);
+			$items = $order->get_items();
+			$str = sprintf('<script>dataLayer.push({
+				"order" : {
+					"id" : %d,
+					"email" : "%s",
+					"country" : "%s",
+					"currency" : "%s",
+					"total" : %f,
+					"discounts" : %f,
+					"shipping-total" : %f,
+					"tax-total" : %f,
+					"est-ship-date" : "%s",
+					"est-delivery-date" : "%s",
+					"has-preorder" : "",
+					"has-digital" : "",
+				},', $order->get_order_number(), $order->billing_email, $order->billing_country, $order->order_currency, $order->order_total, $order->cart_discount, $order->order_shipping, $order->order_tax, date('Y-m-d', strtotime('+1 weekday')), date('Y-m-d', strtotime('+6 weekday')));
+			$str .= '"items" : [';
+			foreach ( $items as $item ) {
+				$str .= sprintf('{
+						"name" : "%s",
+						"price" : %f,
+						"quantity" : %d
+					},', $item['name'], number_format((float)$item['line_total'], 2, '.', ''), $item['qty']);
+			}
+			$str .= ']';
+			$str .= '});</script>';
+		endif;
+		// print script
+		print($str);
+	} else {
+		return false;
+	}
 }
