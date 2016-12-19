c880a99b0cced70d06e2b25aef334ba92561c6d2
alex chousmith <alex@ninthlink.com>
2016-12-19 15:45:10 -0800

    with verifying that escape_url fix for product names, bump this to 2.2.4?

--- gtm4wp.php
+++ gtm4wp.php
@@ -3,7 +3,7 @@
  * Plugin Name: Google Tag Manager 4 WordPress
  * Plugin URI: https://github.com/ninthlink/gtm4wp
  * Description: Add Google Tag Manager to WordPress, with Enhanced eCommerce DataLayer support for WooCommerce
- * Version: 2.2.3
+ * Version: 2.2.4
  * Author: Ninthlink, Inc.
  * Author URI: https://www.ninthlink.com
  * Text Domain: gtm4wp
